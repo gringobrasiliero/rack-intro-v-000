1b5ff23fba6393700462ae72f3b0da6ae8143c46
Nolan Byrnes <nolanehs@gmail.com>
2018-01-12 20:17:34 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World, my name is"
+    resp.write "Hello, my name is Nolan Byrnes"
     resp.finish
   end
 
